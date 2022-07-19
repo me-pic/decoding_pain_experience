a80d5507efeb0773ea91dae4323d043c9dbd1a9e
dylansutterlin <dylan.sutterlin-guindon@umontreal.ca>
2022-07-19 15:58:31 -0400

    changed get_timestamps function
    Simplier version, added .mat file option and returning a df instead of a path

--- pipeline_MVPA/A_data_prep/function_code.py
+++ pipeline_MVPA/A_data_prep/function_code.py
@@ -1,5 +1,13 @@
 
+
+import numpy as np
+import os
+import pandas as pd
+from os.path import exists
+import scipy.io
+
 def get_subj_data(data_dir, prefix = None):
+
     """
     -------------Fonction description----------
 
@@ -15,7 +23,7 @@ def get_subj_data(data_dir, prefix = None):
 
     --------------------------------------------
     """
-    import os
+
 
     #if prefix = None, all the files of the dir will be put in the list
     if prefix == None:
@@ -52,209 +60,112 @@ def get_subj_data(data_dir, prefix = None):
 
 
 
-def adapt_data(subj_path, current_file, subj_name, subj_data, timestamps_path_root):
+def get_timestamps(data_path, subj_name, timestamps_path_root, return_df=None):
 
 
-    #-------------Fonction description----------
+    """
+    Parameters
+    ----------
 
-    #This function is quite specific to the data structure for which it has been created. It will return :
-    # timestamps : a path to the timestamp that goes with a subject data,the design matrix name, under which,
-    #it will later be saved, the mouvement regressor matrix, which will be used to create the design matrix
-    #and finally the run, which is a string that will be used to keep track of the run that is computing, e.g. hyper or hypo.
+    data_path : path to fmri data in order to know in which conditions the subject is
+    timestamps_path_root : root path to the timestamps
+    subj_name : subject's name, e.g. APM_02_H2 to identify the particular cubjects (with different timestamps)
+    return_df : if True, the function returns a pandas dataframe with the timestamps. If None, the path to timestamps will be returned
 
-    #-----------------Variables-----------------
-    #current_file : we assume that this function will be used
-    #while looping over different files that each contains the data
-    #subj_path : path du sujet en cours, e.g. E:\Users\Dylan\Desktop\UdeM_H22\E_PSY3008\data_desmartaux\Nii_test\APM_02_H2
-    #subj_name : nom du sujet, e.g. APM_02_H2
-    #subj_data : a list with all the paths to nii volumes
-    #to_save_root : path where you want to save results for each participant. Note that a file for each subject will be created if it doesn't exist
-    #--------------------------------------------
+    Returns
+    -------
+    timestamps_path : a path to the timestamps file or if return_df =True, a pandas dataFrame which is named df_timestamps
+    """
 
-
-    import numpy as np
-    import os
-    import pandas as pd
-    from os.path import exists
-
-    #///////////paramètres de mouvements/////////////////
-
-    #stockage du fichier des régresseurs de mouvements
-    #on veut dans notre cas le seul fichier dans subj_path qui commence par APM, car c'est le nom du fichier
-    #contenant les régresseurs de mouvements
-
-    movement_reg_sheet = [i for i in os.listdir(subj_path) if i.startswith('APM')]
-    mvmnt_reg_path = subj_path + movement_reg_sheet[0]
-
-    #On les stock dans une liste et on en fait l'extraction ensuite
     movement_reg_list = [i for i in os.listdir(subj_path) if i.startswith('APM')]
     mvmnt_reg_path = os.path.join(subj_path,movement_reg_list[0])
 
     #Read the file
     df_mvmnt_reg_full = pd.read_csv(mvmnt_reg_path, sep= '\s+', header=None)
+#======================================
 
+    #condition file is e.g. 02-Analgesia, it's the file that contains the fMRI volumes
+    condition_file = os.path.basename(os.path.normpath(data_path))
 
-    #////////////////////////////////////////////////////
-    #encodage de la condition
-    if subj_name.endswith('H2'):
-        condition = 'H2'
-    elif subj_name.endswith('H1'):
-        condition = 'H1'
+    if 'Hyperalgesia' in condition_file: #need to return the right timestamps
 
-    else:
-        condition = ''
+        #TIMESTAMPS
+        if subj_name == 'APM_02_H2':
+            if return_df:
+                timestamps = scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_APM02_HYPER.mat'))#.mat option
+            else:
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM02_HYPER.xlsx')#csv option
 
+        elif subj_name == 'APM_05_H2':
+            if return_df:
+                timestamps = scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_APM05_HYPER.mat'))#.mat option
+            else:
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM05_HYPER.xlsx')
 
-    #//////////////////restriction des conditions et des runs/////////////////////
+        elif subj_name == 'APM_17_H2':
+            if return_df:
+                timestamps = scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_APM17_HYPER.mat'))#.mat option
+            else:
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM17_HYPER.xlsx')
 
-        #on veut contrôler si on se trouve dans la condition hyper ou hypo
-        #Si on est dans hyper (02), on va définir le nom du path pour enregistrer la DM, le nom du fichier
-        #Si on est en présence d'exception, soit pour les sujets : APM_02, APM_05, APM_17, APM_20, on va overwrite
-        #les variables du path, du timestamps et du nom du fichier de la DM. Ces sujets ne sont que présent dans la
-        #condition H2
-
-
-    #02 = hyper et 03 = hypo
-    if condition == 'H2':
-
-
-        #on se trouve alors dans la condition HYPER
-        if current_file.startswith('02') is True:
-
-
-           #variables pour la suite
-
-            DM_name = 'DM_HYPER_' + subj_name + '.csv'
-            run = 'HYPER'
-
-            #///////////mouvement regressors -AJUSTEMENTS SELON CONDITION\\\\\\\\\\
-
-            #On souhaite extraire la deuxième moitié de la matrice avec les régresseurs de mouvement
-            #puisqu'on se trouve dans HYPER
-
-
-            df_mvmnt_reg_HYPER = split_reg_lower(df_mvmnt_reg_full, len(subj_data))
-
-
-
-            ##////////////TIMESTAMPS et RÉGRESSEURS DE MOUVEMENTS pour les exceptions//////////////
-            if subj_name == 'APM_02_H2':
-                timestamps = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM02_HYPER.xlsx')
-
-            elif subj_name == 'APM_05_H2':
-                timestamps = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM05_HYPER.xlsx')
-
-            elif subj_name == 'APM_17_H2':
-                timestamps =   os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM17_HYPER.xlsx')
-
-            elif subj_name == 'APM_20_H2':
-                timestamps =   os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM20_HYPER.xlsx')
+        elif subj_name == 'APM_20_H2':
+            if return_df:
+                timestamps = scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_APM20_HYPER.mat'))#.mat option
+            else:
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM20_HYPER.xlsx')
 
             #timestamps HYPER pour les sujets normaux dans H2
-            else :
-                timestamps =   os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_HYPER.xlsx')
-
-            #/////////////return////////////////////
-            return timestamps, DM_name, df_mvmnt_reg_HYPER, run
-
-
-        #on et alors dans la condition 03-HYPO
         else :
-
-            #variable pour la suite et timestamps
-
-            DM_name = 'DM_HYPO_' + subj_name + '.csv'
-            run = 'HYPO'
-
-            #///////////mouvement regressors -AJUSTEMENTS SELON CONDITION\\\\\\\\\\
-
-            #On souhaite extraire la deuxième moitié de la matrice avec les régresseurs de mouvement
-            #puisqu'on se trouve dans HYPER
-
-
-            #from function_split_reg_mvmnt_hypo import split_reg_hypo
-
-            df_mvmnt_reg_HYPO = split_reg_upper(df_mvmnt_reg_full, len(subj_data))
-
-            #//////////////////////////////////
-
-            if subj_name == 'APM_02_H2':
-                timestamps =  os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM02_ANA.xlsx')
-
-            elif subj_name == 'APM_05_H2':
-                timestamps =   os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM05_ANA.xlsx')
-
-            elif subj_name == 'APM_17_H2':
-                timestamps =  os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM17_ANA.xlsx')
-
-            elif subj_name == 'APM_20_H2':
-                timestamps = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM20_ANA.xlsx')
-
-            #timestamps HYPO/ANA pour les sujets normaux dans H2
+            if return_df:
+                timestamps = scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_HYPER.mat'))#.mat option
             else :
-                timestamps = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_ANA.xlsx')
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_HYPER.xlsx')
 
-            ##/////////////////////////////////
-            return timestamps, DM_name, df_mvmnt_reg_HYPO, run
+        #if we are in the Analgesia/hypoalgesia condition
+    elif 'Analgesia' in condition_file:
 
+        if subj_name == 'APM_02_H2':
+            if return_df:
+                timestamps = scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_APM02_ANA.mat'))
+            else:
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM02_ANA.xlsx')
 
-    ## HYPO si on se trouve dans le fichier 03-analgesia
-    elif condition == 'H1':
-        #print('ON EST ENTRÉ DANS LE ELIF DE LA CONDITION H1')
+        elif subj_name == 'APM_05_H2':
+            if return_df:
+                timestamps = scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_APM05_ANA.mat'))
+            else:
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM05_ANA.xlsx')
 
+        elif subj_name == 'APM_17_H2':
+            if return_df:
+                timestamps = scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_APM17_ANA.mat'))
+            else:
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM17_ANA.xlsx')
 
-        if current_file.startswith('02') is True:
+        elif subj_name == 'APM_20_H2':
+            if return_df:
+                timestamps =scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_APM20_ANA.mat'))
+            else:
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_APM20_ANA.xlsx')
 
-            #print('la condition est _' + condition + '_ et on est dans HYPO, 02')
+        #timestamps HYPO/ANA for other 'normal' subjects
+        else :
+            if return_df:
+                timestamps = scipy.io.loadmat(os.path.join(timestamps_path_root,r'ASTREFF_Model6_TxT_model3_multicon_ANA.mat'))
+            else:
+                timestamps_path = os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_ANA.xlsx')
+    #if the return is supposed to be a dataframe
+    if return_df :
 
-            #Variables pour la suite et timestamps
+        df_timestamps = pd.concat([pd.DataFrame(timestamps['onsets']),pd.DataFrame(timestamps['durations']),pd.DataFrame(timestamps['names'])], axis = 1)
+        df_timestamps.columns = ['onsets' , 'durations','Events']
 
-            DM_name = 'DM_HYPO_' + subj_name + '.csv'
-            run = 'HYPO'
-
-            #///////////mouvement regressors -AJUSTEMENTS SELON CONDITION\\\\\\\\\\
-
-
-
-
-            df_mvmnt_reg_HYPO = split_reg_upper(df_mvmnt_reg_full, len(subj_data))
-
-            #timestamps HYPO pour les sujets de H1
-            timestamps =  os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_ANA.xlsx')
-
-            ##/////////////////////////////////
-            return timestamps, DM_name, df_mvmnt_reg_HYPO, run
-
-
-        #Alors on est dans la condition 03-HYPER
-        else:
-
-            #print('la condition est _' + condition + '_ et on est dans HYPER 03')
-
-            #Variables pour la suite et timestamps
-
-            DM_name = 'DM_HYPER_' + subj_name + '.csv'
-            run = 'HYPER'
-
-            #///////////mouvement regressors -AJUSTEMENTS SELON CONDITION\\\\\\\\\\
-
-            #we want to extract the lower part of the mouvement regressor matrix since we are in hyper run
-
-            df_mvmnt_reg_HYPER = split_reg_lower(df_mvmnt_reg_full, len(subj_data))
-
-
-            #timestamps HYPER pour les sujets de H1
-            timestamps =  os.path.join(timestamps_path_root, r'ASTREFF_Model6_TxT_model3_multicon_HYPER.xlsx')
-
-            ##/////////////////////////////////
-            return timestamps, DM_name, df_mvmnt_reg_HYPER, run
-
+        return df_timestamps
+    #else return the path
     else:
+        return timestamps_path
 
-        print('le fichier en cours n\'est pas un sujet appartenant à H1 ou H2')
-
-
-
+#def get_mvmnt_reg(path)
 
 def split_reg_upper(matrix_to_split, target_lenght):
 
@@ -289,9 +200,9 @@ def split_reg_lower(matrix_to_split, target_lenght):
     #--------------------------------------------
 
 
-    inverse_lenght = len(matrix_to_split) - target_lenght
+    #inverse_lenght = len(matrix_to_split) - target_lenght
 
-    split_matrix = matrix_to_split.iloc[inverse_lenght :len(matrix_to_split), :]
+    split_matrix = matrix_to_split.iloc[- target_lenght :]
 
     return split_matrix
 
