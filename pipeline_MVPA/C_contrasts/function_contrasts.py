0ed42306d3c6dd55861aaaae2362a6ec1703a2cb
dylansutterlin <dylan.sutterlin-guindon@umontreal.ca>
2022-07-23 12:44:33 -0400

    Making contrast with multiple runs addition
    Made possible to compute contrast involving many runs of the same blocks

--- pipeline_MVPA/C_contrasts/function_contrasts.py
+++ pipeline_MVPA/C_contrasts/function_contrasts.py
@@ -7,135 +7,8 @@ from nilearn import plotting
 from nilearn.glm.first_level import FirstLevelModel
 from nilearn.image import concat_imgs, mean_img
 import nibabel as nib
+from nilearn.plotting import plot_design_matrix
 
-def glm_contrast_delete_event(design_matrix, subj_result_path, subj_name, fmri_img):
-
-
-    #////////////////Importer les donneés nifti///////////////////////////
-    from nilearn.image import concat_imgs, mean_img
-    import nibabel as nib
-
-    fmri_img_name = subj_name + '_concat_fmri.nii'
-    #print('fmri.shape : {} '.format(fmri_img.shape))
-
-
-    from nilearn.image import mean_img
-
-    #template to project the statistical map
-    #anat_img = nib.load('C:\Users\Dylan\Desktop\UdeM_H22\PSY3008\Avg_Anat')
-    #mean_img = mean_img(fmri_img)
-
-    #####################################################
-    #####################################################
-
-    #///////////MODÈLE/////////////////
-
-    print('====================================')
-    print('COMPUTING GLM for subject ' + subj_name)
-
-    from nilearn.glm.first_level import FirstLevelModel
-
-    fmri_glm = FirstLevelModel(t_r=3, #ok
-                               noise_model='ar1',
-                               standardize=False,
-                               hrf_model='spm',
-                               drift_model='cosine',
-                               high_pass=.00233645)
-
-    fmri_glm = fmri_glm.fit(fmri_img, design_matrices = design_matrix)
-
-    #/////////////basic matrices for contrast//////////////////
-
-    #matrices nulle et identité pour faire les contrastes pour une col
-    #de la design_matrix à la fois
-    identity_matrix = np.eye(design_matrix.shape[1])
-    null_matrix = np.zeros(identity_matrix.shape)
-    #print(null_matrix.shape), print('NULL MATRIX')
-
-    #Création d'un dict ayant comme clé les col de la design_matrix et comme valeur 0 ou 1
-    none_contrasts = dict([(column, np.zeros(identity_matrix.shape[1]))
-      for column in design_matrix.columns])
-
-
-    #contrast for each shock and save it under result_glm_1shock
-    #///////////////contrast loop///////////////////////////
-    #boucle qui prend la colonne dans le eye à l'index de [Nieme titre de la
-    #design_matrix qui contient shock] et qui la met dans un dict
-
-    #faire un dict avec le même nombre de col que la design_matrix. mais remplie de 0
-    #cherche dans la design_matrix pour l'index de la col qui contient 'shock'
-    #si oui :
-        #stocker son index, aller chercher cet index dans la matrice eye
-
-    indx = 0
-    for keys in none_contrasts:
-
-
-        if type(keys) is str:
-            #change according to interst
-            string_interest = 'shock'
-
-            #liste des clés pour retrouver l'index de la colonne d'intérêt
-            key_list = list(none_contrasts)
-
-            #si la clé du dict contient le mot shock
-            if string_interest in keys:
-
-                #///////////preparation contraste///////////
-
-                #aller chercher le nom de la colonne en cours
-                actual_key_name = key_list[indx]
-
-
-                #aller chercher la colonne de contraste d'intérêt à la bonne
-                #position dans identity_matrix
-                contrast_col = identity_matrix[:, indx]
-                #print('La colonne qui a été importée est  {} '.format(contrast_col))
-
-                #mettre une colonne de contraste dans cette clé
-                none_contrasts[actual_key_name] = contrast_col
-                #print(none_contrasts)
-
-                #///////////computing contrast///////////
-                print('==================================')
-                print('COMPUTING CONTRAST FOR : {} '.format(actual_key_name))
-
-
-                from nilearn import plotting
-
-                # compute the contrasts
-                beta_map = fmri_glm.compute_contrast(
-                    contrast_col, output_type='z_score')
-                print('Will save beta_map as a : {} , having shape : {} '.format(type(beta_map),beta_map.shape))
-
-
-                #//////////////Plot option////////////////
-
-                # plot the contrasts as soon as they're generated
-                # the display is overlaid on the mean fMRI image
-                # a threshold of 3.0 is used, more sophisticated choices are possible
-                #plotting.plot_stat_map(
-                    #beta_map, bg_img=mean_img, threshold=3.0, display_mode='z',
-                    #cut_coords=3, black_bg=True, title=actual_key_name)
-
-                #plotting.show()
-
-
-                #////////SAVING OUTPUT//////////////
-                import nibabel as nib
-
-                name_to_save = 'beta_map_' + subj_name + '_' + actual_key_name
-
-                #save la structure nifti, qui est la carte d'activation dans le dossier résultat/participant x
-                nib.save(beta_map, os.path.join(subj_result_path, name_to_save))
-        else:
-            print('KEY IS NOT A STR')
-
-        indx += 1
-
-
-
-            #return beta_map
 
 #=====================================
 #CONTRAST ALL SHOCKS
@@ -149,7 +22,7 @@ def glm_contrast_delete_event(design_matrix, subj_result_path, subj_name, fmri_i
 #fmri_img = r'C:\Users\Dylan\Desktop\UdeM_E22\Projet_Ivado_rainvillelab\result_glm_1shock\APM_02_H2\APM_02_H2_HYPER_concat_fmri.nii'
 ######################
 
-def glm_contrast_1event(design_matrix, subj_result_path, subj_name,fmri_img, run = None):
+def glm_contrast_1event(design_matrix, subj_result_path, subj_name,fmri_img, run_name = None):
 
     #====================
     #Function that takes a design matrix, a path to save, a subject name, a run name and a 4D nii file to conpute contrast
@@ -161,10 +34,8 @@ def glm_contrast_1event(design_matrix, subj_result_path, subj_name,fmri_img, run
     #subj_result_path : a path where you want to save the contrast
     #subj_name : name of the subject
     #fmri_img : a 4D nii file containing data
-    #run : *optionnal. A string containing the name of the actual run that will eb added to the name of the saved contrast file.
+    #run_name : *optionnal. A string containing the name of the actual run that will eb added to the name of the saved contrast file.
         #If not specified, no run string will be included in the saved contrast name
-
-
 
     #////////////////Importer les donneés nifti///////////////////////////
 
@@ -182,7 +53,7 @@ def glm_contrast_1event(design_matrix, subj_result_path, subj_name,fmri_img, run
                                hrf_model='spm',
                                drift_model='cosine',
                                high_pass=.00233645)
-    fmri_glm.fit(fmri_img, design_matrices = design_matrix)
+    fmri_glm = fmri_glm.fit(fmri_img, design_matrices = design_matrix)
 
     #==============
     #identity matrix having shape of number of regressor x number of regressor in the Design matrix
@@ -239,42 +110,25 @@ def glm_contrast_1event(design_matrix, subj_result_path, subj_name,fmri_img, run
                 #plotting.show()
 
                 #---------SAVING OUTPUT--------
-                if run == None:#control if a run string has been provided to include in the name of the file to save
+                if run_name == None:#control if a run_name string has been provided to include in the name of the file to save
                     name_to_save = 'beta_map_' + subj_name  + '_' + actual_key_name
                     #save la structure nifti, qui est la carte d'activation dans le dossier résultat/participant x
                     nib.save(beta_map, os.path.join(subj_result_path, name_to_save))
 
-                if run != None:
-                    name_to_save = 'beta_map_' + subj_name + '_' + run + '_' + actual_key_name
+                if run_name != None:
+                    name_to_save = 'beta_map_' + subj_name + '_' + run_name + '_' + actual_key_name
                     #save la structure nifti, qui est la carte d'activation dans le dossier résultat/participant x
                     nib.save(beta_map, os.path.join(subj_result_path, name_to_save))
                 print('Have saved beta_map as a : {} , having shape : {} , under name : {}'.format(type(beta_map),beta_map.shape, name_to_save))
 
         indx += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     return beta_map
 
-def glm_contrast_all_shocks(design_matrix, subj_result_path, subj_name,fmri_img, run = None):
+def glm_contrast_all_shocks(design_matrix, subj_result_path, subj_name,fmri_img, run_name = None):
 
     #====================
-    #Function that takes a design matrix, a path to save, a subject name, a run name and a 4D nii file to conpute contrast
+    #Function that takes a design matrix, a path to save, a subject name, a run_name name and a 4D nii file to conpute contrast
     #This function is supposed to be used in a loop over many subject file. Therefor, it has as arguments a path to save, a subject's
     #name and a run name to save the file under a name that will take into account such information.
 
@@ -283,7 +137,7 @@ def glm_contrast_all_shocks(design_matrix, subj_result_path, subj_name,fmri_img,
     #subj_result_path : a path where you want to save the contrast
     #subj_name : name of the subject
     #fmri_img : a 4D nii file containing data
-    #run : *optionnal. A string containing the name of the actual run that will eb added to the name of the saved contrast file.
+    #run_name : *optionnal. A string containing the name of the actual run that will eb added to the name of the saved contrast file.
         #If not specified, no run string will be included in the saved contrast name
 
 
@@ -357,7 +211,7 @@ def glm_contrast_all_shocks(design_matrix, subj_result_path, subj_name,fmri_img,
     print('==============')
     print('COMPUTING CONTRAST')
     print('With the CONTRAST VECTOR : {} '.format(contrast_vector))
-    print('For the following REGRESSORS: {} '.format(ls_keys))
+    print('For the Neutral shocks of both runs')
 
     beta_map = fmri_glm.compute_contrast(
          contrast_vector, output_type='z_score')#compute the contrasts with contrast vector
@@ -369,15 +223,15 @@ def glm_contrast_all_shocks(design_matrix, subj_result_path, subj_name,fmri_img,
     plotting.show()
 
      #////////SAVING OUTPUT//////////////
-    if run == None:#control if a run string has been provided to include in the name of the file to save
+    if run_name == None:#control if a run_name string has been provided to include in the name of the file to save
 
         name_to_save = 'beta_map_' + subj_name  + '_all_shocks'
         #save la structure nifti, qui est la carte d'activation dans le dossier résultat/participant x
         nib.save(beta_map, os.path.join(subj_result_path, name_to_save))
 
-    if run != None:
+    if run_name != None:
 
-        name_to_save = 'beta_map_' + subj_name + '_' + run + '_all_shocks'
+        name_to_save = 'beta_map_' + subj_name + '_' + run_name + '_all_shocks'
         #save la structure nifti, qui est la carte d'activation dans le dossier résultat/participant x
         nib.save(beta_map, os.path.join(subj_result_path, name_to_save))
 
@@ -386,3 +240,105 @@ def glm_contrast_all_shocks(design_matrix, subj_result_path, subj_name,fmri_img,
     return beta_map
 
 
+def glm_contrast_runs_N_shocks(design_matrices,all_runs_fmri_img, subj_result_path, subj_name, run_name = None):
+
+    #///////////MODÈLE/////////////////
+    print('==============')
+    print('COMPUTING GLM for all the runs, for subject ' + subj_name)
+
+    #-----comparing DM for all runs
+    from nilearn.plotting import plot_design_matrix
+    import matplotlib.pyplot as plt
+    for design_matrix in design_matrices:
+        plot_design_matrix(design_matrix)
+        plt.show()
+
+    fmri_glm = FirstLevelModel(t_r=3, #ok
+                               noise_model='ar1',
+                               standardize=False,
+                               hrf_model='spm',
+                               drift_model='cosine',
+                               high_pass=.00233645)
+    print(fmri_glm)
+    fmri_glm = fmri_glm.fit(all_runs_fmri_img, design_matrices = design_matrices)#fit for all runs
+
+    #==============
+    #identity matrix having shape of number of regressor x number of regressor in the Design matrix
+    #Each column will serve to encode a 1 in a specifi columns of interest
+    #==============
+    #none contrast is a dictionnary having design matrix column name as key name. The values of each
+    #vector is a 'number of regressor'(design_matrix.shape[1]) long.
+    #the vector in each key is 0 or 1 and serve to encode the contrast in further steps
+
+    identity_matrix = np.eye(design_matrix.shape[1],design_matrix.shape[1])
+    null_matrix = np.zeros(identity_matrix.shape)
+    #print(null_matrix.shape), print('NULL MATRIX')
+
+    none_contrasts = dict([(column, null_matrix[i])
+      for i, column in enumerate(design_matrix.columns)])
+    #print('NONE CONTRAST  {}'.format(none_contrasts))
+    #print('lenght of NONE CONTRAST  {}'.format(len(none_contrasts)))
+
+    contrast_vector = np.zeros((design_matrix.shape[1]))#ones will be added to this vector as we specify which regressor we want to contrast
+
+    #list of all the regressors/keys to keep track of the regressors we've added to contrast
+    ls_keys = []
+    key_list = list(none_contrasts)
+
+    indx = 0
+    for keys in none_contrasts:
+
+        #if the key is str, in order to exclude the drifts and other parameters
+        if type(keys) is str:
+
+            string_interest1 = 'N_'
+            string_interest2 = 'shock'
+
+            if string_interest1 in keys and string_interest2 in keys:
+                print('will add ', keys)
+
+                actual_key_name = key_list[indx]
+                ls_keys.append(keys)#extract the actual regressor/key name with the index position
+                contrast_vector += identity_matrix[:, indx] #sum of the contrast vector with the identity matrix column to stack the ones
+                                                            #in the contrast vector for all regressors of interest
+        indx += 1
+
+    print(ls_keys, ' : All the keys added to contrast')
+
+    #--------computing contrast---------
+    print('==============')
+    print('COMPUTING CONTRAST')
+    print('With the CONTRAST VECTOR : {} '.format(contrast_vector))
+    print('For the following REGRESSORS: {} '.format(ls_keys))
+
+    beta_map = fmri_glm.compute_contrast(
+         contrast_vector, output_type='z_score')#compute the contrasts with contrast vector
+
+     #---------Plot option----------
+    plotting.plot_stat_map(
+        beta_map, threshold=3.0, display_mode='z',
+        cut_coords=3, black_bg=True, title='Neutral shocks Hyper/Hypo')
+    plotting.show()
+
+     #////////SAVING OUTPUT//////////////
+    if run_name == None:#control if a run_name string has been provided to include in the name of the file to save
+
+        name_to_save = 'beta_map_' + subj_name  + '_Neut_shocks'
+        #save la structure nifti, qui est la carte d'activation dans le dossier résultat/participant x
+        nib.save(beta_map, os.path.join(subj_result_path, name_to_save))
+
+    if run_name != None:
+
+        name_to_save = 'beta_map_' + subj_name + '_' + run_name + '_Neut_shocks'
+        #save la structure nifti, qui est la carte d'activation dans le dossier résultat/participant x
+        nib.save(beta_map, os.path.join(subj_result_path, name_to_save))
+
+    print('Have saved beta_map as a : {} , having shape : {} , under name : {}'.format(type(beta_map),beta_map.shape, name_to_save))
+
+    return beta_map
+
+def save_done_file(file_path, done_file_name):
+    done_file=open(os.path.join(file_path,done_file_name), 'w')
+    done_file.write('')
+    done_file.close()
+    print('HAVE WRITTEN : ' + done_file_name)
--- pipeline_MVPA/main_script_1st_level.py
+++ pipeline_MVPA/main_script_1st_level.py
@@ -4,22 +4,22 @@
 #/_!_\ to change according to computer
 
 #main path to data, change according to environment
-#root_dir = r'E:\Users\Dylan\Desktop\UdeM_H22\E_PSY3008\data_desmartaux\Nii'
-#dir_to_save = r'C:\Users\Dylan\Desktop\UdeM_E22\Projet_Ivado_rainvillelab\results_GLM\testing_scripts'
+root_dir = r'E:\Users\Dylan\Desktop\UdeM_H22\E_PSY3008\data_desmartaux\Nii'
+dir_to_save = r'C:\Users\Dylan\Desktop\UdeM_E22\Projet_Ivado_rainvillelab\results_GLM\testing_scripts'
 
 #SERVEUR elm
 
-root_dir = r'/data/rainville/dylan_projet_ivado_decodage/Nii'
-dir_to_save = r'/data/rainville/dylan_projet_ivado_decodage/results/GLM_1st_level_each_shock'
+#root_dir = r'/data/rainville/dylan_projet_ivado_decodage/Nii'
+#dir_to_save = r'/data/rainville/dylan_projet_ivado_decodage/results/GLM_1st_level_each_shock'
 
 # /!\
 #/_!_\ to change according to computer
 #local
-#timestamps_root_path = r'C:\Users\Dylan\Desktop\BAC_neurocog\UM_H22\PSY3008\times_stamps'
+timestamps_root_path = r'C:\Users\Dylan\Desktop\BAC_neurocog\UM_H22\PSY3008\times_stamps'
 
  #elm
-timestamps_path_root = r'/data/rainville/dylan_projet_ivado_decodage/time_stamps'
-
+#timestamps_path_root = r'/data/rainville/dylan_projet_ivado_decodage/time_stamps'
+many_runs=True
 
 import numpy as np
 import os
@@ -45,13 +45,15 @@ for subj_path in ls_subj_path:
     #=================
     subj_name = os.path.basename(os.path.normpath(subj_path)) #extract last part of subj_path to get subject's name
     print(subj_name + ' = subj_name')
-    #=================
+    #----path preparation---
+    #res_folder = input('Enter the name under which to create ')
     #Creating a dir to save, only if it doesn't exists
     if os.path.exists(os.path.join(dir_to_save,subj_name)) is False:
 
         os.mkdir(os.path.join(dir_to_save,subj_name))
     else :
         pass
+
     #=================
     #looking for the regressors' file, which starts with 'APM' and read it as csv
     movement_reg_name = [i for i in os.listdir(subj_path) if i.startswith('APM')]
@@ -62,34 +64,40 @@ for subj_path in ls_subj_path:
     #file names that contains the fMRI volumes.
     str_analgesia ='Analgesia'
     str_hyper = 'Hyperalgesia'
-    test_index = 0
-    #Main loop that will go over the condition's file, and generate a design matrix (DM) and a contrast according to the condition
+
+    if many_runs:
+        design_matrices = []
+        all_fmri_timeseries = []
+
+
     #In that loop, a Timestamps,a DM name,a mouvement regressors dataframe, a DM and statistical maps will be generated and saved
     for condition_file in [i for i in os.listdir(subj_path) if str_analgesia in i or str_hyper in i ]:
         print(condition_file + ' = condition_file')
 
         #-------Extracting fMRI volumes-------
-
         data_path = os.path.join(subj_path,condition_file) #path to the data such as : /subj_01/02-Analgesia/<all nii files>
         subj_volumes= glob.glob(os.path.join(data_path,'sw*'))#extracting all the nii files that start with sw
         print('{} NII files in path {} for subject {}'.format(len(subj_volumes),data_path,subj_name))
         print('lenght of movement regesssor df : {}'.format(len(df_mvmnt_reg_full)))
 
         #-------Extracting timestamps--------
-
         timestamps = A_data_prep.get_timestamps(data_path, subj_name,timestamps_root_path,return_df =True)
         timestamps.sort_values(by=['onset'])
 
-        #-------design matrix name and mouvement regessors--------
+        #----condition and design matrix name-----
+        condition, DM_name = A_data_prep.if_str_in_file(condition_file,subj_name)#checks if str_analgesia or str_hyper is in condition_file
 
-        condition, DM_name = A_data_prep.if_str_in_file(condition_file)#checks if str_analgesia or str_hyper is in condition_file
-        #defining design matrix name and mouvement regessors according to condition
+        #-------movement regessors--------
+        if condition == 'HYPO':
+             mvmnt_reg_df = A_data_prep.split_reg_upper(df_mvmnt_reg_full,len(subj_volumes))
+        elif condition == 'HYPER':
+            mvmnt_reg_df = A_data_prep.split_reg_lower(df_mvmnt_reg_full,len(subj_volumes)) #splitting either the first half or lower half of the mvmnt regressor df according to condition (analg/hyper)
 
         #------DESIGN MATRIX------
-        #check if the DM already exists in path to we save computing time
-        if os.path.exists(os.path.join(dir_to_save, subj_name, DM_name)) is False:
+        if os.path.exists(os.path.join(dir_to_save, subj_name, DM_name)) is False:#check if the DM already exists in path to we save computing time
 
             design_matrix, fmri_time_series = B_design_matrix.create_DM(subj_volumes, timestamps, DM_name, mvmnt_reg_df)
+
             #----------SAVING OUTPUTS------------
             #saving design_matrix and time series
             design_matrix.to_csv(os.path.join(dir_to_save,subj_name,DM_name), index = False)
@@ -100,7 +108,6 @@ for subj_path in ls_subj_path:
             print('Design matrix in condition _{}_  is already existant for : {} '.format(condition, subj_name))
             design_matrix = pd.read_csv(os.path.join(dir_to_save, subj_name, DM_name))
             fmri_img_name = subj_name + '_' + condition + '_fmri_time_series.nii'
-            print(os.path.join(dir_to_save,subj_name, fmri_img_name))
             fmri_time_series = nib.load(os.path.join(dir_to_save,subj_name, fmri_img_name))
 
         #-------Plot option-------
@@ -109,27 +116,42 @@ for subj_path in ls_subj_path:
         #plot_design_matrix(design_matrix)
         #plt.show()
 
-        done_file_name = 'done_contrast_' + condition + '.txt' #define a done_file name to check if it  already exists in file
-
-
-        if os.path.exists(os.path.join(dir_to_save,subj_name,done_file_name)) == False : #to enter the contrast section:
-            #-------CONTRAST-------
-            #contrast for a single shock activation map
-            #contrast.glm_contrast_1event(design_matrix, os.path.join(dir_to_save,subj_name), subj_name, fmri_time_series, run = condition)
-
-            #contrast for all shocks activation map, one for each design matrix will be made
-            #contrast.glm_contrast_all_shocks(design_matrix, os.path.join(dir_to_save,subj_name), subj_name, fmri_time_series, run = condition)
-
-            #-------SAVING-------
-            #write done_contrast_hyper/hypo to keep track of what has been computed
-            done_file=open(os.path.join(dir_to_save,subj_name,done_file_name), 'w')
-            done_file.write('')
-            done_file.close()
-            print('HAVE WRITTEN : ' + done_file_name)
+        if many_runs:
+            condition = 'combined_runs'
+            done_file_name = 'done_contrast_' + condition + '.txt' #define a done_file name to check if it  already exists in file
+            design_matrices.append(design_matrix)
+            all_fmri_timeseries.append(fmri_time_series)
 
         else:
-            print('Contrast : _{}_ has already been done for subject : {} '.format(condition, subj_name))
-        test_index += 1
+            done_file_name = 'done_contrast_' + condition + '.txt' #define a done_file name to check if it  already exists in file
+            if os.path.exists(os.path.join(dir_to_save,subj_name,done_file_name)) == False : #if done_file doesn't exist
+                #-------CONTRAST-------
+                #contrast for a single shock activation map
+                #beta_map = contrast.glm_contrast_1event(design_matrix, os.path.join(dir_to_save,subj_name), subj_name, fmri_time_series, run = condition)
+
+                #contrast for all shocks activation map, one for each design matrix will be made
+                beta_map = contrast.glm_contrast_all_shocks(design_matrix, os.path.join(dir_to_save,subj_name), subj_name, fmri_time_series, run = condition)
+
+                #-------SAVING-------
+                #write done_contrast_hyper/hypo to keep track of what has been computed
+                save_done_file(os.path.join(dir_to_save,subj_name),done_file_name)
+
+            else:
+                print('Contrast : _{}_ has already been done for subject : {} '.format(condition, subj_name))
+
+    if many_runs and os.path.exists(os.path.join(dir_to_save,subj_name,done_file_name)) == False:
+
+        #------All runs neutral shocks-------
+        contrast.glm_contrast_runs_N_shocks(design_matrices,all_fmri_timeseries,
+                                            os.path.join(dir_to_save,subj_name), subj_name, run_name = condition)
+        done_file_name = 'done_contrast_' + condition + '_N_shocks.txt'
+        contrast.save_done_file(os.path.join(dir_to_save,subj_name),done_file_name)
+
+        #------All runs all shocks-------
+        contrast.glm_contrast_all_shocks(design_matrices, all_fmri_timeseries, os.path.join(dir_to_save,subj_name), subj_name, run_name = condition)
+        done_file_name = 'done_contrast_' + condition + '_all_shocks.txt'
+        contrast.save_done_file(os.path.join(dir_to_save,subj_name),done_file_name)
+
 
 #Argument parser
 if __name__ == "__main__":
